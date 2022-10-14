66f6f5b879b60a06c5fa6836fa791e0bc88e5d92
Daniel Gafni <danielgafni16@gmail.com>
2022-10-14 12:40:58 +0200

    fix screensharing & screenshotting

--- dot_config/hypr/executable_hyprland_nvidia_launcher.sh
+++ dot_config/hypr/executable_hyprland_nvidia_launcher.sh
@@ -6,6 +6,6 @@ export XDG_SESSION_TYPE=wayland
 export GBM_BACKEND=nvidia-drm
 export __GLX_VENDOR_LIBRARY_NAME=nvidia
 export WLR_NO_HARDWARE_CURSORS=1
-export XDG_CURRENT_DESKTOP=Unity
+# export XDG_CURRENT_DESKTOP=hyprland
 
-Hyprland
+exec Hyprland
